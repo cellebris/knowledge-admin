04511fa5303f9b653311c4250e794ed48d0d0d84
Adrian Webb <adrian@webb.sh>
2024-12-27 20:30:56 -0500

    Adding an initial reactor initialize script.

---
+++ reactor/initialize.sh
@@ -0,0 +1,7 @@
+#
+#=========================================================================================
+# Project variables
+#
+
+# Set magic variables for directories.
+export __knowledge_admin_project_dir="${__docker_dir}/${1}"
