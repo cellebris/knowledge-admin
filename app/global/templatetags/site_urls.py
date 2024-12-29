73e5a6c3e1f01448adbd287ecfd7aec1dc4a0e2e
Adrian Webb <adrian@webb.sh>
2024-12-29 14:48:42 -0500

    Adding trailing slash compliance in the ui_url and api_url global template tags.

--- app/global/templatetags/site_urls.py
+++ app/global/templatetags/site_urls.py
@@ -7,10 +7,10 @@ register = template.Library()
 @register.simple_tag(name="ui_url")
 def ui_url(*args):
     path = "/".join([str(arg) for arg in args]) if len(args) else ""
-    return f"{settings.BASE_UI_URL}/{path}"
+    return f"{settings.BASE_UI_URL}/{path}".rstrip("/") + "/"
 
 
 @register.simple_tag(name="api_url")
 def api_url(*args):
     path = "/".join([str(arg) for arg in args]) if len(args) else ""
-    return f"{settings.BASE_API_URL}/{path}"
+    return f"{settings.BASE_API_URL}/{path}".rstrip("/") + "/"
