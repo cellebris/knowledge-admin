37006b74f10c0acba662ae26aa5a2bbe3f06a291
Adrian Webb <adrian@webb.sh>
2025-05-22 05:53:48 -0400

    Removing the token embed page in the user url collection.

--- app/users/urls.py
+++ app/users/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path("me/", view=views.DetailView.as_view(), name="me"),
     path("update/", view=views.UpdateFormView.as_view(), name="update"),
     path("token/", view=views.TokenFormView.as_view(), name="token"),
-    path("token/embed", view=views.EmbeddedTokenFormView.as_view(), name="embed_token"),
     path("~token/generate", view=ajax_views.GenerateTokenView.as_view(), name="generate_token"),
     path("~token/revoke", view=ajax_views.RevokeTokenView.as_view(), name="revoke_token"),
     path("~redirect/", view=views.RedirectView.as_view(), name="redirect"),
