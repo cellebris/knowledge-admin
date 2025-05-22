935789f7d881444dc06b454e4dffe05ffab597f8
Adrian Webb <adrian@webb.sh>
2025-05-22 05:37:10 -0400

    Adding a knowledge_admin_run_command function in the reactor admin utility library.

--- reactor/utilities/admin.sh
+++ reactor/utilities/admin.sh
@@ -15,3 +15,18 @@ function knowledge_admin_setup_admin () {
 
   "${__bin_dir}/kubectl" exec -n "$namespace" "$ssh_pod" -- /setup 1>>"$(logfile)" 2>&1
 }
+
+function knowledge_admin_run_command () {
+  namespace="$1"
+  service_name="$2"
+  shift; shift;
+
+  ssh_pod="$(kubectl get pods -n "$namespace" --no-headers -o custom-columns=':metadata.name' | grep "$service_name")"
+
+  debug "Running management command: $@"
+  debug "namespace = ${namespace}"
+  debug "service_name = ${service_name}"
+  debug "ssh_pod = ${ssh_pod}"
+
+  "${__bin_dir}/kubectl" exec -it -n "$namespace" "$ssh_pod" -- /entrypoint ./manage.py "$@"
+}
